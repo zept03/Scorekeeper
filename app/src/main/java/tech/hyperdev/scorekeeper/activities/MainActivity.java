cdf886390af9ac5ab3e0197c11370659f8705bd0
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2017-07-18 00:30:34 +0800

    Initial project structure.

---
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package tech.hyperdev.scorekeeper.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tech.hyperdev.scorekeeper.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
