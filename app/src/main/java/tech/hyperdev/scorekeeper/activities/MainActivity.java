e9b7ce11f0adafd2841c5b3e680d719f38396810
Sept Joshua Rey T. Lozada <sept.lozada@gmail.com>
2017-07-19 00:06:22 -0600

    (3) Modify team names of each score fragment.

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,31 +1,50 @@
 package tech.hyperdev.scorekeeper.activities;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.os.Bundle;
-
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 import tech.hyperdev.scorekeeper.R;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
-
 public class MainActivity extends AppCompatActivity {
 
+    FrameLayout frameOne;
+    FrameLayout frameTwo;
     private FragmentTransaction mFragmentTransaction;
     private FragmentManager mFragmentManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFragmentManager = getSupportFragmentManager();
-        mFragmentTransaction = mFragmentManager.beginTransaction();
-        mFragmentTransaction.replace(R.id.TeamOneFragment, new ScoreFragment());
-        mFragmentTransaction.commit();
+//
+//        mFragmentManager = getFragmentManager();
+//        mFragmentTransaction = mFragmentManager.beginTransaction();
+//        mFragmentTransaction.replace(R.id.TeamOneFragment, new ScoreFragment());
+//        mFragmentTransaction.commit();
+//
+//
+//        mFragmentManager = getFragmentManager();
+//        mFragmentTransaction = mFragmentManager.beginTransaction();
+//        mFragmentTransaction.replace(R.id.TeamTwoFragment, new ScoreFragment());
+//        mFragmentTransaction.commit();
 
 
-        mFragmentManager = getSupportFragmentManager();
-        mFragmentTransaction = mFragmentManager.beginTransaction();
-        mFragmentTransaction.replace(R.id.TeamTwoFragment, new ScoreFragment());
-        mFragmentTransaction.commit();
+        Fragment score_one = ScoreFragment.newInstance("Team 1");
+        Fragment score_two = ScoreFragment.newInstance("Team 2");
+
+        FragmentTransaction one = getFragmentManager().beginTransaction();
+        one.add(R.id.TeamOneFragment, score_one).commit();
+
+        getFragmentManager().beginTransaction()
+                .add(R.id.TeamTwoFragment, score_two).commit();
+
+
+
     }
+
 }
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -1,27 +1,74 @@
 package tech.hyperdev.scorekeeper.fragments;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 import tech.hyperdev.scorekeeper.R;
 
 /**
- * A simple {@link Fragment} subclass.
+ * A simple {@link
+ * } subclass.
  */
 public class ScoreFragment extends Fragment {
 
-    public ScoreFragment() {
+    private int score = 0;
+    private String name;
+
+
+  /*  public ScoreFragment() {
         // Required empty public constructor
-    }
+    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_score, container, false);
+
+        readBundle(getArguments());
+
+        TextView tvName = (TextView) view.findViewById(R.id.tvTeamName);
+        tvName.setText(name);
+
+//
+//        mNameTextView.setText(String.format(name));
+        return view;
+
+    }
+
+    public static ScoreFragment newInstance(String name) {
+        Bundle bundle = new Bundle();
+        bundle.putString("name", name);
+
+        ScoreFragment fragment = new ScoreFragment();
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
+
+    private void readBundle(Bundle bundle) {
+        if (bundle != null) {
+            name = bundle.getString("name");
+        }
+    }
+
+
+
+//    @Override
+//    public void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putInt("score", score);
+//    }
+
+
+
+
+    public void SetText() {
+
     }
 
 }
